43480fd68b9f552ea35a1d7ccbc826470fa33fe1
davidgn2k <davidgn2k@gmail.com>
2023-01-01 18:11:14 -0600

    Error fix at index.php

--- index.php
+++ index.php
@@ -24,10 +24,6 @@ $mname = $_REQUEST['form_mname'];
 $license = $_REQUEST['form_license'];
 $university = $_REQUEST['form_university'];
 $faculty = $_REQUEST['form_faculty'];
-
-if (isset($_POST['action'])) {
-  $sql = "INSERT INTO profesor (nombre, cedulaProfesional, grado, paterno, materno) VALUES($name, $license, $degree, $lname, $mname)" ;
-}
 ?>
 
 <!DOCTYPE html>
@@ -70,37 +66,40 @@ if (isset($_POST['action'])) {
     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
   </symbol>
   </svg>
+  
+  <?php
+  if (isset($_POST['action'])) {
+    $sql = "INSERT INTO profesor (nombre, cedulaProfesional, grado, paterno, materno) VALUES($name, $license, $degree, $lname, $mname)" ;
+    if(mysqli_query($conn, $sql)){
 
-  <?php if(mysqli_query($conn, $sql)){
+      echo '<div class="alert alert-success d-flex align-items-center" role="alert">
+      <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+      <div>
+        Operación Exitosa. La información ha sido insertada en la base de datos.
+      </div>
+    </div> 
 
-            echo '<div class="alert alert-success d-flex align-items-center" role="alert">
-            <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-            <div>
-              Operación Exitosa. La información ha sido insertada en la base de datos.
-            </div>
-          </div> 
+    
+    setTimeout(function() {
+      window.location.reload();
+  }, 2500);';
 
-          
-          setTimeout(function() {
-            window.location.reload();
-        }, 2500);';
+  } else{
+      echo '<div class="alert alert-danger d-flex align-items-center" role="alert">
+      <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+      <div>
+        Error. La información no ha podido ser insertada en la base de datos. Intente de nuevo.
+      </div>
+    </div>
 
-        } else{
-            echo '<div class="alert alert-danger d-flex align-items-center" role="alert">
-            <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-            <div>
-              Error. La información no ha podido ser insertada en la base de datos. Intente de nuevo.
-            </div>
-          </div>
-
-          setTimeout(function() {
-            window.location.reload();
-        }, 2500)';
-        }
-         
-        // Close connection
-        mysqli_close($conn);
-
+    setTimeout(function() {
+      window.location.reload();
+    }, 2500)';
+  }
+   
+  // Close connection
+  mysqli_close($conn);
+  }
         ?>
         
       <!-- Navbar start -->
