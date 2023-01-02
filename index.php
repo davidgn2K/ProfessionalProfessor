1678c6d4467287bb33ceff63f74c21f41ee94cb9
davidgn2k <davidgn2k@gmail.com>
2023-01-01 18:02:24 -0600

    Implemented functional from at index

--- index.php
+++ index.php
@@ -1,9 +1,33 @@
 <?php
 
+$host = "localhost";
 $user = "ProfessionalProfessorAdmin";
 $password = "12345";
 $database = "professionalProfessor";
 
+ // servername => localhost
+        // username => root
+        // password => 12345
+        // database name => professionalProfessor
+$conn = mysqli_connect($host, $user, "12345", $database);
+
+// Check connection
+if($conn === false){
+  die("ERROR: Could not connect. "
+      . mysqli_connect_error());
+}
+
+$degree =  $_REQUEST['form_degree'];
+$name = $_REQUEST['form_name'];
+$lname =  $_REQUEST['form_lname'];
+$mname = $_REQUEST['form_mname'];
+$license = $_REQUEST['form_license'];
+$university = $_REQUEST['form_university'];
+$faculty = $_REQUEST['form_faculty'];
+
+if (isset($_POST['action'])) {
+  $sql = "INSERT INTO profesor (nombre, cedulaProfesional, grado, paterno, materno) VALUES($name, $license, $degree, $lname, $mname)" ;
+}
 ?>
 
 <!DOCTYPE html>
@@ -35,6 +59,50 @@ $database = "professionalProfessor";
   </head>
   <body>
 
+  <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+  <symbol id="check-circle-fill" viewBox="0 0 16 16">
+    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+  </symbol>
+  <symbol id="info-fill" viewBox="0 0 16 16">
+    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+  </symbol>
+  <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
+    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+  </symbol>
+  </svg>
+
+  <?php if(mysqli_query($conn, $sql)){
+
+            echo '<div class="alert alert-success d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+            <div>
+              Operación Exitosa. La información ha sido insertada en la base de datos.
+            </div>
+          </div> 
+
+          
+          setTimeout(function() {
+            window.location.reload();
+        }, 2500);';
+
+        } else{
+            echo '<div class="alert alert-danger d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+            <div>
+              Error. La información no ha podido ser insertada en la base de datos. Intente de nuevo.
+            </div>
+          </div>
+
+          setTimeout(function() {
+            window.location.reload();
+        }, 2500)';
+        }
+         
+        // Close connection
+        mysqli_close($conn);
+
+        ?>
+        
       <!-- Navbar start -->
       <nav class="navbar navbar-expand-md navbar-light border-bottom border-dark" style="background-color: #FFFFFF;">
         <a class="col-1 navbar-brand" href="#">
@@ -73,12 +141,12 @@ $database = "professionalProfessor";
         </div>  
       </div> 
 
-      <form class="mx-3 my-5">
+      <form action="" class="mx-3 my-5">
         <div class="form-row">
 
           <div class="col-md-3 mb-3">
             <label class="mr-sm-2" for="inlineFormCustomSelect01">Grado</label>
-            <select class="custom-select mr-sm-2" id="inlineFormCustomSelect01">
+            <select class="custom-select mr-sm-2" id="inlineFormCustomSelect01" name="form_degree">
               <option value="1" selected>Licenciado</option>
               <option value="2">Maestro</option>
               <option value="3">Doctor</option>
@@ -88,26 +156,26 @@ $database = "professionalProfessor";
 
           <div class="col-md-3 mb-3">
             <label for="validationDefault01">Nombre</label>
-            <input type="text" class="form-control" id="validationDefault01" placeholder="Alberto" value="" required>
+            <input type="text" class="form-control" id="validationDefault01" placeholder="Alberto" value="" name="form_name" required>
           </div>
 
           <div class="col-md-3 mb-3">
             <label for="validationDefault02">Apellido Paterno</label>
-            <input type="text" class="form-control" id="validationDefault02" placeholder="Arenas" value="" required>
+            <input type="text" class="form-control" id="validationDefault02" placeholder="Arenas" value="" name="form_lname" required>
           </div>
           <div class="col-md-3 mb-3">
             <label for="validationDefault03">Apellido Materno</label>
-            <input type="text" class="form-control" id="validationDefault03" placeholder="Islas" value="">
+            <input type="text" class="form-control" id="validationDefault03" placeholder="Islas" name="form_mname" value="">
           </div>
         </div>
         <div class="form-row">
           <div class="col-md-3 mb-3">
             <label for="validationDefault04">Cédula Profesional</label>
-            <input type="text" class="form-control" id="validationDefault04" placeholder="23456789" required>
+            <input type="text" class="form-control" id="validationDefault04" placeholder="23456789" name="form_license" required>
           </div>
           <div class="col-md-5 mb-3">
             <label class="mr-sm-2" for="inlineFormCustomSelect02">Universidad</label>
-            <select class="custom-select mr-sm-2" id="inlineFormCustomSelect02">
+            <select class="custom-select mr-sm-2" id="inlineFormCustomSelect02" name="form_university">
               <option value="1" selected>Universidad Nacional Autónoma de México</option>
               <option value="2">Instituto Tecnológico de Estudios Superiores de Monterrey</option>
               <option value="3">Instituto Tecnológico Autónomo de México</option>
@@ -115,7 +183,7 @@ $database = "professionalProfessor";
           </div>
           <div class="col-md-4 mb-3">
             <label class="mr-sm-2" for="inlineFormCustomSelect03">Facultad</label>
-            <select class="custom-select mr-sm-2" id="inlineFormCustomSelect03">
+            <select class="custom-select mr-sm-2" id="inlineFormCustomSelect03" name="form_faculty">
               <option value="1" selected>Op1</option>
               <option value="2">Op2</option>
               <option value="3">Op3</option>
@@ -126,7 +194,7 @@ $database = "professionalProfessor";
           <div class="col-md-3">
           </div>
           <div class="col-md-6 mt-5">
-            <button class="button-27" type="submit">Confirmar&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-chevron-right"></i> </button>
+            <button class="button-27" type="submit" name="submitButton">Confirmar&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-chevron-right"></i> </button>
           </div>
           <div class="col-md-3">
           </div>
